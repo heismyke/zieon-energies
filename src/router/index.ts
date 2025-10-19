57c063ff94ffe7db3083ad5f948cda3275487746
heismyke <mickienorman5@gmail.com>
2025-10-19 16:20:35 +0100

    fix: linter errors

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import type { RouteRecordRaw } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
+import type { RouteRecordRaw } from "vue-router";
 
 const routes: RouteRecordRaw[] = [
   {
-    path: '/',
-    name: 'Home',
-    component: () => import('../views/HomeView.vue'),
+    path: "/",
+    name: "Home",
+    component: () => import("../views/HomeView.vue"),
   },
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-  scrollBehavior(to, from, savedPosition) {
+  scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     } else {
-      return { top: 0, behavior: 'smooth' }
+      return { top: 0, behavior: "smooth" };
     }
   },
-})
+});
 
-export default router
+export default router;
